6ac646d06371918e1e24375d1853aa054dea3b39
pawanlal <pawanlalganesh@gmail.com>
2020-09-02 16:57:26 +0545

    Small Changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,48 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-    render() {
-        return (            
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <Link to="/author">
-                        <a className="navbar-brand">Library</a>
-                    </Link>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                        <Link to="/author">
-                            <a className="nav-link" >Authors  <span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link to="/publisher">
-                            <a className="nav-link" >Publisher  <span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li> 
-                    <li className="nav-item active">
-                        <Link to="/book">
-                            <a className="nav-link" >Book  <span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li>  
-                    <li className="nav-item active">
-                        <Link to="/request">
-                            <a className="nav-link" >Request  <span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li>   
-                    <li className="nav-item active">
-                        <Link to="/issue">
-                            <a className="nav-link" >Book Issued  <span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li>                                                                    
-                    </ul>                    
-                </div>
-            </nav>      
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <Link to="/author">
+            <a href="/" className="navbar-brand">
+              Library
+            </a>
+          </Link>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <Link to="/author">
+                <a href="/" className="nav-link">
+                  Authors <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/publisher">
+                <a href="/" className="nav-link">
+                  Publisher <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/book">
+                <a href="/" className="nav-link">
+                  Book <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/request">
+                <a href="/" className="nav-link">
+                  Request <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link to="/issue">
+                <a href="/" className="nav-link">
+                  Book Issued <span className="sr-only">(current)</span>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
