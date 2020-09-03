07722d23dc805dd15182d6469416dc1e8f3c017b
pawanlal <pawanlalganesh@gmail.com>
2020-09-03 14:20:57 +0545

    minor change

--- src/actions/Classroom.js
+++ src/actions/Classroom.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-import { GET_CLASSROOMS, DELETE_CLASSROOMS, GET_USERS, ADD_CLASSROOM_SUCCESS } from './types';
+import {
+  GET_CLASSROOMS,
+  DELETE_CLASSROOMS,
+  GET_USERS,
+  ADD_CLASSROOM_SUCCESS,
+} from "./types";
 
 //GET CLASSROOMS
 
-export const getClassrooms = () => dispatch => {
-    axios
-    .get('http://127.0.0.1:8000/api/classroom/classroom/')
+export const getClassrooms = () => (dispatch) => {
+  axios
+    .get("http://127.0.0.1:8000/api/classroom/classroom/")
     .then((res) => {
-        dispatch({
-            type: GET_CLASSROOMS,
-            payload: res.data,
-        });
+      dispatch({
+        type: GET_CLASSROOMS,
+        payload: res.data,
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -19,42 +24,41 @@ export const getClassrooms = () => dispatch => {
 //DELETE CLASSROOM
 
 export const deleteClassroom = (id) => (dispatch) => {
-    axios
+  axios
     .delete(`http://127.0.0.1:8000/api/classroom/classroom/${id}/`)
     .then((res) => {
-        dispatch({
-            type: DELETE_CLASSROOMS,
-            payload: id,
-        });
+      dispatch({
+        type: DELETE_CLASSROOMS,
+        payload: id,
+      });
     })
     .catch((err) => console.log(err));
-}
-
+};
 
 //ADD CLASSROOM
 
 export const addClassroom = (classroom) => (dispatch) => {
-    axios
-    .post("http://127.0.0.1:8000/api/classroom/classroom",classroom)
+  axios
+    .post("http://127.0.0.1:8000/api/classroom/classroom", classroom)
     .then((res) => {
-        dispatch({
-            type: ADD_CLASSROOM_SUCCESS,
-            payload: res.data,
-        });
+      dispatch({
+        type: ADD_CLASSROOM_SUCCESS,
+        payload: res.data,
+      });
     })
     .catch((err) => console.log(err));
-}
+};
 
 //GET USERS
 
-export const getUsers = () => dispatch => {
-    axios
-    .get('http://127.0.0.1:8000/api/account/')
+export const getUsers = () => (dispatch) => {
+  axios
+    .get("http://127.0.0.1:8000/api/account/")
     .then((res) => {
-        dispatch({
-            type: GET_USERS,
-            payload: res.data,
-        });
+      dispatch({
+        type: GET_USERS,
+        payload: res.data,
+      });
     })
     .catch((err) => console.log(err));
 };
