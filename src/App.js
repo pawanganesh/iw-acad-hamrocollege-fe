3938af514f4f8c17f50877bc99a5c9ad260b683d
pawanlal <pawanlalganesh@gmail.com>
2020-09-05 18:31:02 +0545

    small changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/layout/Header'
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/layout/Header";
 // import Dashboard from './components/assign/Dashboard'
-import {Provider} from 'react-redux'
-import store from './store'
-import Author from './components/Author/Author'
-import Publisher from './components/Publisher/Publisher'
-import Book from './components/Book/Book'
-import Request from './components/Request/Request'
-import Issue from './components/Issue/Issue'
-
-
-
-
+import { Provider } from "react-redux";
+import store from "./store";
+import Author from "./components/Author/Author";
+import Publisher from "./components/Publisher/Publisher";
+import Book from "./components/Book/Book";
+import Request from "./components/Request/Request";
+import Issue from "./components/Issue/Issue";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>        
-        <Header/>  
-        {/* <div className='container'>
-          
-        </div>  */}
-        <Switch>            
-            <Route exact path="/author" component={Author} />
-            <Route exact path="/publisher" component={Publisher} />  
-            <Route exact path="/book" component={Book} />  
-            <Route exact path="/request" component={Request} />
-            <Route exact path="/issue" component={Issue} />          
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/author" component={Author} />
+          <Route exact path="/publisher" component={Publisher} />
+          <Route exact path="/book" component={Book} />
+          <Route exact path="/request" component={Request} />
+          <Route exact path="/issue" component={Issue} />
         </Switch>
       </Router>
     </Provider>
-    
   );
 }
 
@@ -53,7 +45,6 @@ export default App;
 // import PrivateRoute from "./common/PrivateRoute";
 // import { loadUser } from "../actions/auth";
 // import store from "../store";
-
 
 // import Author from '../components/Author/Author'
 // import Publisher from '../components/Publisher/Publisher'
@@ -88,10 +79,10 @@ export default App;
 //               />
 //               {/* Added start */}
 //                  <Route exact path="/author" component={Author} />
-                //  <Route exact path="/publisher" component={Publisher} />  
-                //  <Route exact path="/book" component={Book} />  
-                //  <Route exact path="/request" component={Request} />
-                //  <Route exact path="/issue" component={Issue} />  
+//  <Route exact path="/publisher" component={Publisher} />
+//  <Route exact path="/book" component={Book} />
+//  <Route exact path="/request" component={Request} />
+//  <Route exact path="/issue" component={Issue} />
 
 //               {/* Added End */}
 //             </Switch>
@@ -104,4 +95,3 @@ export default App;
 // }
 
 // export default App;
-
--- src/components/Author/Authorform.js
+++ src/components/Author/Authorform.js
@@ -1,69 +1,62 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
-import {addAuthors} from '../../actions/authors'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addAuthors } from "../../actions/authors";
 
+class Authorform extends Component {
+  state = {
+    firstname: "",
+    lastname: "",
+  };
 
+  static propTypes = {
+    addAuthors: PropTypes.func.isRequired,
+  };
 
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { firstname, lastname } = this.state;
+    const authors = { firstname, lastname };
+    this.props.addAuthors(authors);
+  };
 
- class Authorform extends Component {
-     state = {
-         firstname: '',
-         lastname: '',
-         
-         
-         
-     }
+  render() {
+    const { firstname, lastname } = this.state;
+    return (
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Author</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Firstname </label>
+            <input
+              className="form-control"
+              type="text"
+              name="firstname"
+              onChange={this.onChange}
+              value={firstname}
+            />
+          </div>
+          <div className="form-group">
+            <label>Lastname </label>
+            <input
+              className="form-control"
+              type="text"
+              name="lastname"
+              onChange={this.onChange}
+              value={lastname}
+            />
+          </div>
 
-     static propTypes = {
-         addAuthors: PropTypes.func.isRequired
-     }
-
-     onChange=e=> this.setState({ [e.target.name]: e.target.value })
-     onSubmit = e => {
-         e.preventDefault()
-         const{firstname, lastname} = this.state
-         const authors= {firstname, lastname}
-         this.props.addAuthors(authors)         
-     }
-
-
-    render() {
-        const { firstname, lastname } = this.state
-        return (
-            <div className="card card-body mt-4 mb-4">
-                <h2>Add Author</h2>
-                <form onSubmit={this.onSubmit}>                    
-                    <div className='form-group'>
-                        <label>Firstname </label>
-                        <input 
-                        className="form-control"
-                        type="text"
-                        name="firstname"
-                        onChange={this.onChange}
-                        value={firstname}
-                        />    
-                    </div>
-                    <div className='form-group'>
-                        <label>Lastname </label>
-                        <input 
-                        className="form-control"
-                        type="text"
-                        name="lastname"
-                        onChange={this.onChange}
-                        value={lastname}
-                        />    
-                    </div>                        
-                                       
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
-                            Submit
-                        </button>
-                    </div>
-                </form>                                
-            </div>
-        )
-    }
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default connect(null, {addAuthors}) (Authorform)
+export default connect(null, { addAuthors })(Authorform);
